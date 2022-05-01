3ec9ad0d8d52ce2575be263873f0772fd1ba92cc
kimyoosang <yoo99485@gmail.com>
2022-05-01 20:00:31 +0900

    :sparkles: ADD: deploy ERC20 Token

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+// const GLDToken = artifacts.require('GLDToken.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  // deployer.deploy(GLDToken, 1000000000000);
 };
